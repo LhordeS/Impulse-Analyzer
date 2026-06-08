b1eee4128aed7f71b65dd0352bfb40a9bafe8db7
Alain Mimeault <lhorde@msn.com>
2026-06-08 17:46:45 +0900

    Clean notebook to make it gh-readable

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import yfinance as yf
 
-df_nq = yf.download("NQ=F", interval="1m", period="7d")
-df_es = yf.download("ES=F", interval="1m", period="7d")
+df_nq = yf.download("NQ-F", interval="1m", period="7d")
 
-df_nq.to_csv("nq_1m_data.csv")
+df_nq.index = df_nq.index.tz_convert("america/New_York")
+df_nq["date"] = df_nq.index.date
+df_nq.columns = df_nq.columns.get_level_values(0)
 
-# def detect_opening_inpulse(day_df, atr_value, threshold_fraction):
+rth = df_nq.between_time("09:30", "15:59").copy()
+rth["date"] = rth.index.date
+one_day = rth[rth["date"] == rth["date"].iloc[0]]
+opening_window = one_day.between_time("09:30", "09:59")
 
-print(df_nq)
